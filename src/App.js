73f17eaba6119ea3f012a14dd1d35045b19ea41d
Eliboyev Doston <doston1721@gmail.com>
2020-10-23 22:44:39 +0500

    I add some new file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/header/header.js'
-// import Recommend from './Components/recommend/recommend';
-// import Mostread from './Components/mostread/mostread';
-// import Attention from './Components/attention/attention';
-// import Footer from './Comp  onents/footer/footer'
-// import Reklama from './Components/Reklama/Reklama'
-// import Bestcompany from './Components/Best Companies/bestcompany';
+import Recommend from './Components/recommend/recommend';
+import Mostread from './Components/mostread/mostread';
+import Attention from './Components/attention/attention';
+import Footer from './Components/footer/footer'
+import Reklama from './Components/Reklama/Reklama'
+import Bestcompany from './Components/Best Companies/bestcompany';
 import Miniheader from './Components/Miniheader/miniheader'
 import Posttab from './Components/PostTab/Posttab'
 import News from './Components/News/news'
@@ -14,13 +14,14 @@ function App() {
   return (
     <div>
     <Header/>
+    <Miniheader/>
+    <Posttab/>
+    {/* <Mostread/> */}
     {/* <Footer/> */}
     {/* <Reklama/> */}
     {/* <Bestcompany/> */}
     {/* <Recommend/> */}
-    {/* <Mostread/> */}
     {/* <Attention/> */}
-    <Miniheader/><br/>
     </div> 
 
   );
--- src/Components/PostTab/Posttab.js
+++ src/Components/PostTab/Posttab.js
@@ -71,6 +71,66 @@ export default class Posttab extends Component {
                             </article>
                             </Route>
                         </li>
+                        <li className='post-ul-li'>
+                            <Route>
+                            <article>
+                                <header className='post-header'>
+                                <Link className='post-link'>
+                                <img src={logo1} className='post-link-logo'/>
+                                <span className='post-link-name'>PatientZero</span>
+                                </Link>
+                                <span className='post-header-time'>вчера в 08:04</span>
+                                </header>
+                                <h2 className='post-title'>
+                                    <Link className='post-title-link'>
+                                        Почему единственный вид деревьев не победил все остальные?
+                                    </Link>
+                                </h2>
+                                <ul className='post-habs'>
+                                    <Link className='post-link-hubs'><li>Математика</li></Link>
+                                    <Link className='post-link-hubs'><li>Научно-популярное,</li></Link>
+                                    <Link className='post-link-hubs'><li>Экология</li></Link>
+                                </ul>
+                                <div className='post-body'>
+                                    <img src={bodyimg} className='body-img'/>
+                                    <div className='body-text'>
+                                        <p>С началом пандемии большинство разработчиков Кремниевой долины начали трудиться из дома. Довольно быстро они стали задаваться вопросом: зачем платить бешеные цены за аренду недвижимости, если домашний офис можно организовать где угодно? Средняя плата за аренду односпальной квартиры в Сан-Франциско составляет $3629. А, скажем, в Чикаго, Атланте или Филадельфии — тоже вполне крупных городах — она будет около $1650. Зачем платить лишние $2 тысячи, больше $20 тысяч в год, чтобы сидеть в своём стуле чуть поближе к штаб-квартире компании?</p>
+                                        <p>Многие задумались о переезде (по крайней мере, на время). Но хитрые финансисты Кремниевой долины своего не упустят. И теперь крупные компании начинают вводить новую стратегию: модифицируемая зарплата!</p>
+
+                                    </div>
+                                    <Link className='body-link'>
+                                    Читать далее
+                                    </Link>
+                                </div>
+                                <div className='post-footer'>
+                                        <span className='post-footer-item'>
+                                            <FaBookmark></FaBookmark>
+                                            <span style={{color:'#6c9007',marginLeft:'8px'}}>+298</span>
+                                        </span>
+                                        <Link className='post-footer-link1' style={{textDecoration:'none'}}>
+                                        <span className='post-footer-item'>
+                                            <FaBookmark></FaBookmark>
+                                            <span style={{marginLeft:'8px'}}>112</span>
+
+                                        </span>
+                                        </Link>
+                                        <span className='post-footer-item'>
+                                            <FaEye></FaEye>
+                                            <span style={{marginLeft:'8px'}} >41k</span>
+
+                                        </span>
+                                        
+                                        <Link className='post-footer-link2'  style={{textDecoration:'none'}}>
+                                        <span className='post-footer-item' >
+                                            <FaCommentAlt></FaCommentAlt>
+                                            <span style={{marginLeft:'8px'}}>215</span>
+
+                                        </span>
+                                        </Link>
+                                </div>
+                            </article>
+                            </Route>
+                        </li>
                     </ul>
                 </div>
         )
